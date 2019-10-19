5a81650547e189b19c90fcf8ae17caa17b142df9
Matias <mhuenul@fi.uba.ar>
2019-10-19 19:43:49 -0300

    Ahora usa map y grep para filtrar lista de links

--- downloader.pl
+++ downloader.pl
@@ -4,6 +4,8 @@ use LWP::Simple;
 use HTML::Parse;
 use HTML::Element;
 
+die "ERROR: Se debe pasar un código de materia como parámetro" if (!defined $ARGV[0]);
+
 my $codigo_materia = substr($ARGV[0], 0, 2) . ":" . substr($ARGV[0], 2);
 
 my $url = "http://wiki.foros-fiuba.com.ar";
@@ -14,14 +16,7 @@ die "ERROR: No se pudo obtener contenido" if (!defined $content);
 
 my $parsed = HTML::Parse::parse_html($content);
 
-my @media_links = ();
-
-foreach (@{$parsed->extract_links()}) {
-	my $link = $_->[0];
-	if (substr($link, 0, 7) eq "/_media") {
-		push(@media_links, $link);
-	}
-}
+my @media_links = map { $_->[0] } grep { substr($_->[0], 0, 7) eq "/_media" } @{$parsed->extract_links()};
 
 foreach my $link (@media_links) {
 	my $filename = substr($link, rindex($link, ":") + 1);
